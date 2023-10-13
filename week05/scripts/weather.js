c244f6cd55a15e61f62920bbcbf41068be69f2b7
Use your GitHub username <mahomedleticia@gmail.com>
2023-10-12 21:01:07 -0300

    update

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -21,7 +21,7 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.weather[0].temp}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
